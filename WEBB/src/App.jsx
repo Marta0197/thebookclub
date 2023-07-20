5505cdc39dbe985ddd4dba52234c06e3df8102dd
MartaFR2801 <martafrancoramos28@gmail.com>
2023-07-20 09:30:27 +0200

    archivos

--- WEBB/src/App.jsx
+++ WEBB/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import Login from "./views/Login/Login";
+import Profile from "./views/Profile/Profile";
+import BookCreator from "./views/BookCreator/BookCreator";
+import Signup from "./views/Signup/Signup";
+import MyBooks from "./views/MyBooks/MyBooks";
+//import DetailBook from "./views/DetailBook/DetailBook";
+import Landing from "./views/Landing/Landing";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
 
+  // Check if the current route path is "login"
+  const isLanding = location.pathname === "/";
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {!isLanding && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+         <Route
+          path="create-book"
+          element={
+            <ProtectedRoute>
+              <BookCreator />
+            </ProtectedRoute>
+          }
+        />
+            <Route
+          path="my-books"
+          element={
+            <ProtectedRoute>
+              <MyBooks />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
